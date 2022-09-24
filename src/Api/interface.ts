e4a669d975dde9e719ec2f919457b534b92596d2
rigood <rigood@naver.com>
2022-09-24 19:46:24 +0900

    🏷️ Add interface for Detail-data

--- src/Api/interface.ts
+++ src/Api/interface.ts
@@ -12,17 +12,51 @@ export interface IContent {
   poster_path: string;
 }
 
+export interface IGenre {
+  id: number;
+  name: string;
+}
+
+export interface INetwork {
+  name: string;
+  logo_path: string;
+  origin_country: string;
+}
+
+export interface ISeason {
+  air_date: string;
+  episode_count: number;
+  id: string;
+  name: string;
+  overview: string;
+  poster_path: string;
+  season_number: number;
+}
+
 export interface IDetails {
   id: number;
-  title: string;
-  name: string;
-  genres: {
-    id: number;
-    name: string;
-  };
+  title: string; // movie
+  name: string; // tv
+  backdrop_path: string;
+  poster_path: string;
+  status: string;
+  genres: IGenre[];
+  overview: string;
+  vote_average: string;
+  original_language: string;
   runtime: number; // movie
-  number_of_episodes: number;
-  number_of_seasons: number;
+  episode_run_time: number; // tv
+  release_date: string; // movie
+  first_air_date: string; // tv
+  last_air_date: string; // tv
+  imdb_id: string; // movie
+  homepage: string; // tv
+  adult: boolean; // movie
+  video: boolean; // movie
+  networks: INetwork[]; // tv
+  seasons: ISeason[]; // tv
+  number_of_seasons: number; // tv
+  number_of_episodes: number; // tv
 }
 
 export interface ICast {
