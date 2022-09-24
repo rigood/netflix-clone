f6d5668f19c6b54b51b3aa95e8e4fa9766d35a2c
rigood <rigood@naver.com>
2022-09-24 18:23:39 +0900

    💄 Create theme and GlobalStyle

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+/* Data-fetching */
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+/* Theme-color */
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme";
+
+/* Global-style */
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+/* Components */
 import App from "./App";
+
+const GlobalStyle = createGlobalStyle`
+${reset};
+* {
+  box-sizing: border-box;
+}
+body {
+  overflow-x: hidden;
+  background-color: black;
+  font-family: 'Noto Sans KR', sans-serif;
+  font-weight: 700; // (300light, 500medium, 700bold)
+  color: ${(props) => props.theme.white.darker};
+  line-height: 1.5;
+}
+a {
+  text-decoration: none;
+  color: inherit;
+}
+`;
 
 const client = new QueryClient();
 
@@ -10,7 +41,10 @@ const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 
 root.render(
   <QueryClientProvider client={client}>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
     <ReactQueryDevtools position="bottom-right" />
   </QueryClientProvider>
 );
---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  red: "#E51013",
+  green: "#46D369",
+  black: {
+    veryDark: "#141414",
+    darker: "#181818",
+    lighter: "#2F2F2F",
+  },
+  white: {
+    lighter: "#fff",
+    darker: "#e5e5e5",
+  },
+};
