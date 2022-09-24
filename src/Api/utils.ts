cb0d78a70aef7739dfa2a05bdfc6a987cbe743a1
rigood <rigood@naver.com>
2022-09-24 20:05:50 +0900

    🍱 Add no-image png files and implement makeImgPath function

--- src/Api/utils.ts
+++ src/Api/utils.ts
@@ -1,1 +1,9 @@
-export {};
+export function makeBackdropPath(id: string, format?: string) {
+  if (id) return `https://image.tmdb.org/t/p/${format ? format : "original"}/${id}`;
+  else return process.env.PUBLIC_URL + "/assets/no-image-landscape.png";
+}
+
+export function makePosterPath(id: string, format?: string) {
+  if (id) return `https://image.tmdb.org/t/p/${format ? format : "original"}/${id}`;
+  else return process.env.PUBLIC_URL + "/assets/no-image-portrait.png";
+}
