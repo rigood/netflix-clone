f6aa842358a5832271e31109cf20431f6432d9bf
rigood <rigood@naver.com>
2022-10-19 11:18:22 +0900

    ✨ Add prev, next button to Slider

--- src/Components/Slider.tsx
+++ src/Components/Slider.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 /* Routing */
 import { useNavigate } from "react-router-dom";
@@ -16,23 +16,60 @@ import { getBackdropPath } from "../Api/utils";
 import { useSetRecoilState } from "recoil";
 import { modalState } from "../atom";
 
+/* Slice list */
+import { useState } from "react";
+
+/* Icons */
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+
 /* Styling */
 
 const Container = styled.div`
   margin: 3vw 0;
 `;
 
+const RowWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 60px auto 60px;
+`;
+
+const Btn = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 60px;
+  border: none;
+  background: none;
+  ${(props) =>
+    !props.disabled &&
+    css`
+      color: white;
+      opacity: 0.5;
+      font-size: 1.5vw;
+      &:hover {
+        opacity: 1;
+        font-size: 1.8vw;
+        border: none;
+        outline: none;
+        cursor: pointer;
+      }
+    `}
+`;
+
+const PrevBtn = styled(Btn)``;
+const NextBtn = styled(Btn)``;
+
 const Row = styled(motion.div)`
   display: grid;
   grid-template-columns: repeat(6, 1fr);
   gap: 10px;
   width: 100%;
-  padding-inline: 4%;
 `;
 
 const Title = styled.h3`
   margin-bottom: 1vw;
-  padding-inline: 4%;
+  padding-inline: 60px;
   font-size: 1.4vw;
 `;
 
@@ -65,19 +102,41 @@ function Slider({ section, category, title, list }: ISliderProps) {
     sliceIndex = 0;
   }
 
+  /* Slice list */
+  const offset = 6;
+  const [index, setIndex] = useState(0);
+  const listLength = list?.length!;
+  const maxIndex = Math.floor(listLength / offset) - 1;
+
+  /* Prev, Next */
+  const decreaseIndex = () => {
+    setIndex((prev) => (prev === 0 ? 0 : prev - 1));
+  };
+  const increaseIndex = () => {
+    setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
+  };
+
   return (
     <Container>
       <Title>{title}</Title>
-      <AnimatePresence>
-        <Row>
-          {list
-            ?.slice(sliceIndex)
-            .slice(0, 6)
-            .map((content) => (
-              <Box key={content.id} bg={getBackdropPath(content.backdrop_path, "w500")} onClick={() => onBoxClick(content.id)} />
-            ))}
-        </Row>
-      </AnimatePresence>
+      <RowWrapper>
+        <PrevBtn onClick={decreaseIndex} disabled={index === 0}>
+          <FontAwesomeIcon icon={faAngleLeft} />
+        </PrevBtn>
+        <AnimatePresence>
+          <Row>
+            {list
+              ?.slice(sliceIndex)
+              .slice(offset * index, offset * index + offset)
+              .map((content) => (
+                <Box key={content.id} bg={getBackdropPath(content.backdrop_path, "w500")} onClick={() => onBoxClick(content.id)} />
+              ))}
+          </Row>
+        </AnimatePresence>
+        <NextBtn onClick={increaseIndex}>
+          <FontAwesomeIcon icon={faAngleRight} />
+        </NextBtn>
+      </RowWrapper>
     </Container>
   );
 }
