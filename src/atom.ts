31c85dcf06b462d267e46b544a5b7ae97bdb5aa6
rigood <rigood@naver.com>
2022-11-24 09:09:57 +0900

    ✨ Save MyList in LocalStorage

--- src/atom.ts
+++ src/atom.ts
@@ -1,5 +1,10 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
 import { IContent } from "./Api/interface";
+
+const { persistAtom } = recoilPersist({
+  key: "myList",
+});
 
 export const modalState = atom({
   key: "modal",
@@ -9,9 +14,11 @@ export const modalState = atom({
 export const myMovieAtom = atom<IContent[]>({
   key: "myMovie",
   default: [],
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const myTvAtom = atom<IContent[]>({
   key: "myTv",
   default: [],
+  effects_UNSTABLE: [persistAtom],
 });
