1f9a13b90a58811d0fea0f0f7950238446f3ca91
rigood <rigood@naver.com>
2022-11-27 23:15:05 +0900

    ♻️ Refactor countdown and redirect logic

--- src/Pages/NotFound.tsx
+++ src/Pages/NotFound.tsx
@@ -1,6 +1,30 @@
-import styled from "styled-components";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+function NotFound() {
+  const navigate = useNavigate();
+  const INITIAL_SECONDS = 3;
+  const [count, setCount] = useState(INITIAL_SECONDS);
+
+  useEffect(() => {
+    const countdown = setInterval(() => setCount((prev) => prev - 1), 1000);
+    if (count === 0) navigate("/");
+
+    return () => clearInterval(countdown);
+  }, [count]);
+
+  return (
+    <Wrapper>
+      <h1>해당 페이지를 찾을 수 없습니다.</h1>
+      <p>
+        <strong>{count}초</strong> 뒤 메인 화면으로 이동합니다.
+      </p>
+    </Wrapper>
+  );
+}
+
+export default NotFound;
 
 const Wrapper = styled.div`
   padding: 200px 60px 60px 60px;
@@ -16,31 +40,3 @@ const Wrapper = styled.div`
     }
   }
 `;
-
-function NotFound() {
-  const INITIAL_SECONDS = 3;
-  const [seconds, setSeconds] = useState(INITIAL_SECONDS);
-
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    setTimeout(() => {
-      navigate("/");
-    }, INITIAL_SECONDS * 1000);
-
-    setInterval(() => {
-      setSeconds((prev) => prev - 1);
-    }, 1000);
-  }, []);
-
-  return (
-    <Wrapper>
-      <h1>해당 페이지를 찾을 수 없습니다.</h1>
-      <p>
-        <strong>{seconds}초</strong> 뒤 Home 화면으로 이동합니다.
-      </p>
-    </Wrapper>
-  );
-}
-
-export default NotFound;
