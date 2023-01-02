c883079fdf28ac0befbd59d2d434c8ac0ba91262
rigood <rigood@naver.com>
2023-01-02 23:39:41 +0900

    🚀 Install deploy packages

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,38 +5,43 @@ import MyList from "./pages/MyList";
 import Search from "./pages/Search";
 import NotFound from "./pages/NotFound";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Navigate to="browse/movie" />,
+        },
+        {
+          path: "browse/:section",
+          element: <Browse />,
+        },
+        {
+          path: "search/:section",
+          element: <Search />,
+        },
+        {
+          path: "mylist",
+          element: <MyList />,
+          children: [
+            {
+              path: ":section",
+              element: <MyList />,
+            },
+          ],
+        },
+        {
+          path: "*",
+          element: <NotFound />,
+        },
+      ],
+    },
+  ],
   {
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <Navigate to="browse/movie" />,
-      },
-      {
-        path: "browse/:section",
-        element: <Browse />,
-      },
-      {
-        path: "search/:section",
-        element: <Search />,
-      },
-      {
-        path: "mylist",
-        element: <MyList />,
-        children: [
-          {
-            path: ":section",
-            element: <MyList />,
-          },
-        ],
-      },
-      {
-        path: "*",
-        element: <NotFound />,
-      },
-    ],
-  },
-]);
+    basename: process.env.PUBLIC_URL,
+  }
+);
 
 export default router;
